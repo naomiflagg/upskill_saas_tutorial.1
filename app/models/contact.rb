8cbd682bbf1b1961cd85d0c49f635574cf3d9ca1
Naomi Flagg <neffly511@gmail.com>
2020-04-07 17:54:05 +0000

    Added validation check for contact form email field

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
   validates :comments, presence: true
 end
