59cc60ee8fa14aa1963b7d4fbb18bb176c270054
Naomi Flagg <neffly511@gmail.com>
2020-04-07 19:14:56 +0000

    Added code comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+  # Contact form validations
   validates :name, presence: true
   validates :email, presence: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
   validates :comments, presence: true
