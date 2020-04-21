d4b7ab800b31c1aeb23104104d9d6969a1e1a85a
Naomi Flagg <neffly511@gmail.com>
2020-04-21 21:38:07 +0000

    Fixed Stripe payment issues by correcting typo

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,49 +2,38 @@
 //Document ready.
 $(document).on('turbolinks:load', function(){
   var theForm = $('#pro_form');
-  var submitBtn = $('#form-signup-btn');
-  
+  var submitBtn = $('#form-submit-btn');
   //Set Stripe public key.
   Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
-
-  //When user clicks form submit button,
-  submitBtn.cick(function(event){
+  //When user clicks form submit btn,
+  submitBtn.click(function(event){
     //prevent default submission behavior.
     event.preventDefault();
-    //Change text button to processing and disable link 
-    //so that user knows to wait while page processes.
     submitBtn.val("Processing").prop('disabled', true);
-    
     //Collect the credit card fields.
     var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-    
     //Use Stripe JS library to check for card errors.
     var error = false;
-    
     //Validate card number.
     if(!Stripe.card.validateCardNumber(ccNum)) {
       error = true;
-      alert('The credit card number appears to be invalid.');
+      alert('The credit card number appears to be invalid');
     }
-    
     //Validate CVC number.
     if(!Stripe.card.validateCVC(cvcNum)) {
       error = true;
-      alert('The CVC number appears to be invalid.');
+      alert('The CVC number appears to be invalid');
     }
-    
     //Validate expiration date.
     if(!Stripe.card.validateExpiry(expMonth, expYear)) {
       error = true;
-      alert('The expiration date appears to be invalid.');
+      alert('The expiration date appears to be invalid');
     }
-    
     if (error) {
-      //If there are card errors, don't send to Stripe
-      //and re-enable submit button.
+      //If there are card errors, don't send to Stripe.
       submitBtn.prop('disabled', false).val("Sign Up");
     } else {
       //Send the card info to Stripe.
@@ -57,16 +46,13 @@ $(document).on('turbolinks:load', function(){
     }
     return false;
   });
-  
   //Stripe will return a card token.
   function stripeResponseHandler(status, response) {
-    //Get the token from the response
+    //Get the token from the response.
     var token = response.id;
-  
-  //Inject card token as hidden field into form.
-  theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
-  
-  //Submit form to our rails app.
-  theForm.get(0).submit();
+    //Inject the card token in a hidden field.
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+    //Submit form to our Rails app.
+    theForm.get(0).submit();
   }
 });
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   
   belongs_to :plan
   
-  attr_accessor :token
+  attr_accessor :stripe_card_token
   # If Pro user passes validations (email, password, etc.),
   # then call Stripe and tell Stripe to set up a subscription
   # upon charging the customer's card.
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   # Store customer.id as the customer token and save the user.
   def save_with_subscription
     if valid?
-      customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
